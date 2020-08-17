aee969e426f0f48e5d48deeaa99f242233f56582
Noah Golub <noahgolub2@gmail.com>
2020-08-17 15:01:18 -0700

    removed comments

--- src/kdf.rs
+++ src/kdf.rs
@@ -1,7 +1,5 @@
 use argon2::{self, Config};
 
-// Method borrowed from rust-bitcoin's endian module.
-/// PBKDF2-HMAC-SHA512 implementation using bitcoin_hashes.
 pub(crate) fn argon(passphrase: &[u8], salt: &[u8]) -> Vec<u8> {
     argon2::hash_raw(passphrase, salt, &Config::default()).expect(
         "Failure 
